6e6ff59bfc5646ac0aba8fe754fe42ae3f2649ed
Rebecca Bartels <rebeccakbartels@gmail.com>
2018-05-19 19:00:27 -0400

    no errors and got first markup point up.

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
-
-
+//variables 
 
 var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
   attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
 });
-var map = L.map('map', { measureControl:true}).setView([51.1657, 10.4515], 4);
+
+  // initialize map object with view
+
+var map = L.map('map', { measureControl:true}, {drawControl: true}).setView([51.1657, 10.4515], 4);
 L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
   attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community', 
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    attribution: 'L.geoJSON(geojsonFeature).addTo(map);'
 }).addTo(map);
-L.Control.measureControl().addTo(map);
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
-measureControl.addTo(map);
 
+//var leafletDraw = require(leaflet-draw);
 
 
 Esri_WorldImagery.addTo(map);
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
 
-  // initialize map object with view
-  window.map = L.map('map').setView([0, 0], 3);
-  // add tile layer
-  L.tileLayer('http://a.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
   // add multiple data layers (defined above ... sloppy!)
-  window.data1 = L.geoJson(data1).addTo(map);
-  window.data2 = L.geoJson(data2).addTo(map);
+  //window.data1 = L.geoJson(data1).addTo(map);
+  //window.data2 = L.geoJson(data2).addTo(map);
+
+
   // toggle variable to use in togglePoints();
   window.toggle = false;
 
@@ -37,4 +38,21 @@ function togglePoints() {
   }
   toggle = !toggle;
 }
-window.onload = init();
+//window.onload = init();
+
+var geojsonFeature = {
+    "type": "Feature",
+    "properties": {
+        "name": "Coors Field",
+        "amenity": "Baseball Stadium",
+        "popupContent": "This is where the Rockies play!"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [52.5097, 13.3728]
+    }
+};
+
+
+
+
