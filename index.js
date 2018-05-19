ef57143426a84b8763b929a0d14e779941ed4d66
Rebecca Bartels <rebeccakbartels@gmail.com>
2018-05-19 17:18:48 -0400

    base map complete

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 
 
-var map = L.map('map').setView([51.1657, 10.4515], 4);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+
+var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+  attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+});
+var map = L.map('map', { measureControl:true}).setView([51.1657, 10.4515], 4);
+L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+  attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community', 
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
-
+L.Control.measureControl().addTo(map);
 L.marker([51.5, -0.09]).addTo(map)
     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
     .openPopup();
 measureControl.addTo(map);
 
+
+
+Esri_WorldImagery.addTo(map);
 
   // initialize map object with view
   window.map = L.map('map').setView([0, 0], 3);
