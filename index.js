d7a1788bacafedac4369e862610312696a486298
Rebecca Bartels <rebeccakbartels@gmail.com>
2018-05-21 21:00:52 -0400

    added geojson npm

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/
     attribution: 'L.geoJSON(geojsonFeature).addTo(map);'
 }).addTo(map);
 
-//var leafletDraw = require(leaflet-draw);
-
+//var leafletDraw = require(leaflet-draw); - not sure if needed yet 
+//icons 
 //worms 
 L.marker([49.6341, 8.3507]).addTo(map)
     .bindPopup("Worms, 1171-1175")
@@ -20,8 +20,42 @@ L.marker([49.6341, 8.3507]).addTo(map)
 L.marker([49.9929, 8.2473]).addTo(map)
     .bindPopup("Mainz")
 
+//geojson 
+var states = [{
+    "type": "Feature",
+    "properties": {"party": "Republican"},
+    "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+            [49.6341, 8.3507],
+            [-97.22,  48.98],
+            [-96.58,  45.94],
+            [-104.03, 45.94],
+            [-104.05, 48.99]
+        ]]
+    }
+}, {
+    "type": "Feature",
+    "properties": {"party": "Democrat"},
+    "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+            [49.6341, 8.3507],
+            [-102.06, 40.99],
+            [-102.03, 36.99],
+            [-109.04, 36.99],
+            [-109.05, 41.00]
+        ]]
+    }
+}];
 
 
-
-
+L.geoJSON(states, {
+    style: function(feature) {
+        switch (feature.properties.party) {
+            case 'Locations of Hausen': return {color: "#ff0000"};
+            case 'The Jewish Pogroms':   return {color: "#0000ff"};
+        }
+    }
+}).addTo(map);
 
